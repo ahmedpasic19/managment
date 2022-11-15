20208d3568060087f193dcfd055f429f4ec9bc9c
Ahmed_Pasic <ahmedpasic15@gmail.com>
2022-11-15 22:35:02 +0100

    Persist dat with redux-persist

--- client/src/app/store.ts
+++ client/src/app/store.ts
@@ -3,6 +3,9 @@ import userReducer from '../features/users/userSlice'
 import taskReducer from '../features/tasks/taskSlice'
 import authReducer from '../features/auth/authSlice'
 
+//Persist data
+import { persistStore } from 'redux-persist'
+
 export const store = configureStore({
   reducer: {
     users: userReducer,
@@ -10,6 +13,8 @@ export const store = configureStore({
     auth: authReducer,
   },
 })
+
+export const persistore = persistStore(store)
 
 export type AppDispatch = typeof store.dispatch
 export type RootState = ReturnType<typeof store.getState>
